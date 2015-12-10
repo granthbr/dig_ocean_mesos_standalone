822ccb7dec19e7334d7d1ae353b918622604c8b3
Brandon Grantham <bgrantham@lyris.com>
2015-12-10 09:15:33 -0800

    fixed script. removed backslashes

--- standalone.sh
+++ standalone.sh
@@ -3,7 +3,7 @@ IP=`wget http://ipecho.net/plain -O - -q ; echo`
 apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF  
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')  
 CODENAME=$(lsb_release -cs)  
-echo  \"deb http://repos.mesosphere.io/ubuntu trusty main\" | tee /etc/apt/sources.list.d/mesosphere.list  
+echo  "deb http://repos.mesosphere.io/ubuntu trusty main" | tee /etc/apt/sources.list.d/mesosphere.list  
 apt-get -y update 
 sudo apt-get -y install mesosphere  
 sudo dpkg --configure -a  
@@ -13,7 +13,7 @@ echo Cluster01 |  tee /etc/mesos-master/cluster
 echo 1 |  tee /etc/mesos-master/quorum  
 echo $IP |  tee /etc/mesos-slave/hostname  
 echo $IP |  tee /etc/mesos-slave/ip  
-echo \"cgroups/cpu,cgroups/mem\" |  tee /etc/mesos-slave/isolation  
-echo zk:\//$IP:2181/mesos | tee /etc/mesos/zk  
+echo "cgroups/cpu,cgroups/mem" |  tee /etc/mesos-slave/isolation  
+echo zk://$IP:2181/mesos | tee /etc/mesos/zk  
 echo 1 |  tee /etc/zookeeper/conf/myid  
 reboot
